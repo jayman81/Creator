81c1aef8f8dbdd8faa2902b845e6c250b01233b6
Juergen Bis <juergen.bis@rise-world.com>
2018-03-13 13:00:48 +0100

    add a headerfile for main

---
+++ src/main.hpp
@@ -0,0 +1,21 @@
+/*
+ * main.hpp
+ *
+ *  Created on: Mar 13, 2018
+ *      Author: jurgen
+ */
+
+#ifndef MAIN_HPP_
+#define MAIN_HPP_
+
+
+uint8_t createWeapon();
+void createArmor();
+void createShield();
+void createPotion();
+void createOther();
+
+
+
+
+#endif /* MAIN_HPP_ */
